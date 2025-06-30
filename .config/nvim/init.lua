aac0189ed75616b9e8c16203f12e83780bc87d9d
Valentin Reichenspurner <v.reichenspurner@tum.de>
2025-07-01 00:03:05 +0200

    multicurser plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -248,7 +248,7 @@ rtp:prepend(lazypath)
 require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"NMAC427/guess-indent.nvim", -- Detect tabstop and shiftwidth automatically
-
+	"mg979/vim-visual-multi", -- enable multicurser editing
 	-- NOTE: Plugins can also be added by using a table,
 	-- with the first argument being the link and the following
 	-- keys can be used to configure plugin behavior/loading/etc.
